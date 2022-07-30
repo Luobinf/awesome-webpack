acac602e04e77e26c063bbfd140c77b389adf7b2
Luobinf <1096422441@qq.com>
2022-07-30 10:17:48 +0800

    修改 webpack.config.js 配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
-const WebpackRunPlugin = require('./plugins/webpack-run-plugin')
-const WebpackDonePlugin = require('./plugins/webpack-done-plugin')
-const path = require('path')
+const WebpackRunPlugin = require("./plugins/webpack-run-plugin");
+const WebpackDonePlugin = require("./plugins/webpack-done-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    path.resolve(__dirname, 'loaders/logger-loader.js'),
-                    path.resolve(__dirname, 'loaders/logger2-loader.js'),
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    path.resolve(__dirname, 'loaders/style-loader.js')
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new WebpackRunPlugin(),
-        new WebpackDonePlugin()
-    ]
-}
+  entry: {
+    home: "./src/home.js",
+    about: "./src/about.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve(__dirname, "loaders/logger-loader.js"),
+          path.resolve(__dirname, "loaders/logger2-loader.js"),
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [path.resolve(__dirname, "loaders/style-loader.js")],
+      },
+    ],
+  },
+  plugins: [new WebpackRunPlugin(), new WebpackDonePlugin()],
+};
