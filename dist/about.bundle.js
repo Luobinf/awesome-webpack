004f8befa61afcf647095a999262736a2eecc12a
Luobinf <1096422441@qq.com>
2022-07-31 09:22:56 +0800

    验证打包结果

--- dist/about.bundle.js
+++ dist/about.bundle.js
@@ -1,39 +1,44 @@
-(() => {
-  var __webpack_modules__ = {
-    "./src/title.js": (module, exports, require) => {
-      module.exports = "title";
-    },
-    "./src/about.js": (module, exports, require) => {
-      let title = require("./src/title.js");
 
-      console.log("about:", title);
-    },
-  };
-  // The module cache
-  var __webpack_module_cache__ = {};
+  (() => {
+    var __webpack_modules__ = {
+      
+            "./src/title.js": ( module,exports,require) => {
+                module.exports = "title";
+              }
+          ,
+            "./src/about.js": ( module,exports,require) => {
+                let title = require("./src/title.js");
 
-  // The require function
-  function __webpack_require__(moduleId) {
-    // Check if module is in cache
-    var cachedModule = __webpack_module_cache__[moduleId];
-    if (cachedModule !== undefined) {
-      return cachedModule.exports;
+console.log("about:", title);
+              }
+          
+    };
+    // The module cache
+    var __webpack_module_cache__ = {};
+  
+    // The require function
+    function __webpack_require__(moduleId) {
+      // Check if module is in cache
+      var cachedModule = __webpack_module_cache__[moduleId];
+      if (cachedModule !== undefined) {
+        return cachedModule.exports;
+      }
+      // Create a new module (and put it into the cache)
+      var module = (__webpack_module_cache__[moduleId] = {
+        // no module.id needed
+        // no module.loaded needed
+        exports: {},
+      });
+  
+      // Execute the module function
+      __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  
+      // Return the exports of the module
+      return module.exports;
     }
-    // Create a new module (and put it into the cache)
-    var module = (__webpack_module_cache__[moduleId] = {
-      // no module.id needed
-      // no module.loaded needed
-      exports: {},
-    });
-
-    // Execute the module function
-    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-
-    // Return the exports of the module
-    return module.exports;
-  }
-  // startup
-  // Load entry module and return exports
-  // This entry module can't be inlined because the eval devtool is used.
-  var __webpack_exports__ = __webpack_require__("./src/about.js");
-})();
+    // startup
+    // Load entry module and return exports
+    // This entry module can't be inlined because the eval devtool is used.
+    var __webpack_exports__ = __webpack_require__("./src/about.js");
+  })();
+  
